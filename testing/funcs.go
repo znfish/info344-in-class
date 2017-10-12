e1efcf9af09ee58c4af88675e767d446caed712f
Dave Stearns <dlsinfo@uw.edu>
2017-10-11 18:55:00 -0700

    another buggy function to test

--- testing/funcs.go
+++ testing/funcs.go
@@ -24,6 +24,23 @@ func trace(message string) {
 	}
 }
 
+//Reverse returns the reverse of the string passed as `s`
+func Reverse(s string) string {
+    //convert string to a slice so we can manipulate it
+    //since strings are immutable, this creates a copy of
+    //the string so we won't be modifying the original
+    chars := []byte(s)
+
+    //starting from each end, swap the values in the slice
+    //elements, stopping when we get to the middle
+    for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+        chars[i], chars[j] = chars[j], chars[i]
+    }
+
+    //return the reversed slice as a string
+    return string(chars)
+}
+
 //GetGreeting returns a greeting for a `name`.
 //if name is zero-length, or all spaces,
 //"World" will be used instead
--- testing/funcs_test.go
+++ testing/funcs_test.go
@@ -4,11 +4,18 @@ import (
 	"testing"
 )
 
+func TestReverse(t *testing.T) {
+
+}
+
 func TestGetGreeting(t *testing.T) {
+
 }
 
 func TestParseSize(t *testing.T) {
+
 }
 
 func TestLateDaysConsume(t *testing.T) {
+
 }
