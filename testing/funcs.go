2ce3b322aafa5c17fc1ff7cf702ce2f9bc5def22
Christy Lu <hyl9@uw.edu>
2017-10-12 14:54:37 -0700

    test

--- testing/funcs.go
+++ testing/funcs.go
@@ -26,19 +26,19 @@ func trace(message string) {
 
 //Reverse returns the reverse of the string passed as `s`
 func Reverse(s string) string {
-    //convert string to a slice so we can manipulate it
-    //since strings are immutable, this creates a copy of
-    //the string so we won't be modifying the original
-    chars := []byte(s)
+	//convert string to a slice so we can manipulate it
+	//since strings are immutable, this creates a copy of
+	//the string so we won't be modifying the original
+	chars := []rune(s)
 
-    //starting from each end, swap the values in the slice
-    //elements, stopping when we get to the middle
-    for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
-        chars[i], chars[j] = chars[j], chars[i]
-    }
+	//starting from each end, swap the values in the slice
+	//elements, stopping when we get to the middle
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
 
-    //return the reversed slice as a string
-    return string(chars)
+	//return the reversed slice as a string
+	return string(chars)
 }
 
 //GetGreeting returns a greeting for a `name`.
@@ -46,7 +46,7 @@ func Reverse(s string) string {
 //"World" will be used instead
 func GetGreeting(name string) string {
 	if len(name) == 0 {
-		name := "World"
+		name = "World"
 		trace(fmt.Sprintf("defaulting name to `%s`", name))
 	}
 	return fmt.Sprintf("Hello, %s!", name)
@@ -59,6 +59,9 @@ func GetGreeting(name string) string {
 //will be set to zero
 func ParseSize(size string) *Size {
 	nums := strings.Split(size, "x")
+	if len(nums) < 2 {
+		return &Size{}
+	}
 	width, _ := strconv.Atoi(nums[0])
 	height, _ := strconv.Atoi(nums[1])
 	return &Size{width, height}
@@ -86,9 +89,11 @@ func NewLateDays() *LateDays {
 func (ld *LateDays) Consume(netID string) int {
 	//if netID is not yet a key in the map
 	//add an entry with the value 4
-	if ld.entries[netID] == 0 {
+	if _, found := ld.entries[netID]; !found {
 		ld.entries[netID] = DefaultLateDays
 	}
-	ld.entries[netID]--
+	if ld.entries[netID] > 0 {
+		ld.entries[netID]--
+	}
 	return ld.entries[netID]
 }
--- testing/funcs_test.go
+++ testing/funcs_test.go
@@ -5,17 +5,100 @@ import (
 )
 
 func TestReverse(t *testing.T) {
+	cases := []struct {
+		name         string
+		input        string
+		exepectedOut string
+	}{ //initializer
+		{
+			name:         "empty string",
+			input:        "",
+			exepectedOut: "",
+		},
+		{
+			name:         "single character",
+			input:        "a",
+			exepectedOut: "a",
+		},
+		{
+			name:         "two characters",
+			input:        "ab",
+			exepectedOut: "ba",
+		},
+		{
+			name:         "stressed",
+			input:        "stressed",
+			exepectedOut: "desserts",
+		},
+		{
+			name:         "high ascii",
+			input:        "Hello, 世界",
+			exepectedOut: "界世 ,olleH",
+		},
+	}
 
+	for _, c := range cases {
+		if output := Reverse(c.input); output != c.exepectedOut {
+			t.Errorf("%s: got %s bit expected %s", c.name, output, c.exepectedOut)
+		}
+	}
 }
 
 func TestGetGreeting(t *testing.T) {
+	cases := []struct {
+		name         string
+		input        string
+		exepectedOut string
+	}{
+		{
+			name:         "empty",
+			input:        "",
+			exepectedOut: "Hello, World!",
+		},
+		{
+			name:         "Name = Christy",
+			input:        "Christy",
+			exepectedOut: "Hello, Christy!",
+		},
+	}
 
+	for _, c := range cases {
+		if output := GetGreeting(c.input); output != c.exepectedOut {
+			t.Errorf("%s: got %s but expected %s", c.name, output, c.exepectedOut)
+		}
+	}
 }
 
 func TestParseSize(t *testing.T) {
+	cases := []struct {
+		name         string
+		input        string
+		exepectedOut *Size
+	}{
+		{
+			name:         "empty string",
+			input:        "",
+			exepectedOut: &Size{},
+		},
+	}
 
+	for _, c := range cases {
+		if output := ParseSize(c.input); output.Height !=
+			c.exepectedOut.Height || output.Width != c.exepectedOut.Width {
+			t.Errorf("%s: got %v but expected %v", c.name, output, c.exepectedOut)
+		}
+	}
 }
 
 func TestLateDaysConsume(t *testing.T) {
-
+	ld := NewLateDays()
+	for i := 3; i > -10; i-- {
+		expectedLateDays := i
+		if expectedLateDays < 0 {
+			expectedLateDays = 0
+		}
+		if output := ld.Consume("test"); output != expectedLateDays {
+			t.Errorf("iteration %d: got %d but expected %d", i, output, expectedLateDays)
+		}
+	}
 }
